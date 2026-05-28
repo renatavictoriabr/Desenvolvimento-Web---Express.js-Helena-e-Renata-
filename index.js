f41509e394914cb06446a405d669890423a108b0
renatavictoriabr <lirarenata53@gmail.com>
2026-05-28 12:35:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/filmes/:id/:nome', (req, res) => {
-  const id = req.params.id;
-  const nome = req.params.nome;
-  res.send(`ID do filme: ${id}   
- Nome do filme: ${nome}`);
+app.get('/buscar', (req, res) => {
+  const nome = req.query.nome;
+  res.send(`Buscando por: ${nome}`);
 });
 
 app.listen(
